9d1f08783083f69f4948977d8e4f387bc6c9da9a
coderhisham <imhish4m@gmail.com>
2024-06-24 22:51:38 +0530

    Updated

---
+++ Web/MERN/FoodDeliveryApp/frontend/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food from here..</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima illum
+          eaque mollitia maiores iure, ea non quaerat provident dolores
+          temporibus debitis quisquam dignissimos pariatur a officiis iusto
+          beatae rerum vitae.e
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
